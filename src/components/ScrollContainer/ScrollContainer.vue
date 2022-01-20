f752e46e63c84ead0a02f5408684c34c6f83a1c8
zmtlwzy <1025302051@qq.com>
2022-01-20 16:15:59 +0800

    feat(component): add scrollbar comp

---
+++ src/components/ScrollContainer/ScrollContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <n-scrollbar :builtin-theme-overrides="scrollBarThemeOverrides">
+    <slot />
+  </n-scrollbar>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'ScrollContainer',
+  };
+</script>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+  import { getNaiveCssVars } from '/@/composables/core/useNaiveInternal';
+
+  const props = defineProps<{
+    inverted?: boolean;
+  }>();
+
+  const { color, colorHover } = getNaiveCssVars('Scrollbar', true);
+  const scrollBarThemeOverrides = computed(() => {
+    return props.inverted ? { color, colorHover } : undefined;
+  });
+</script>
