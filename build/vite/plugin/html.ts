592748a16358991db9a6052808228c480e5598fa
zmtlwzy <1025302051@qq.com>
2022-04-16 18:26:41 +0800

    chore: update deps

--- build/vite/plugin/html.ts
+++ build/vite/plugin/html.ts
@@ -2,7 +2,6 @@
  * Plugin to minimize and use ejs template syntax in index.html.
  * https://github.com/anncwb/vite-plugin-html
  */
-import type { PluginOption } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import pkg from '../../../package.json';
 import { GLOB_CONFIG_FILE_NAME } from '../../constant';
@@ -16,7 +15,7 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
     return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
   };
 
-  const htmlPlugin: PluginOption[] = createHtmlPlugin({
+  const htmlPlugin = createHtmlPlugin({
     minify: isBuild,
     inject: {
       // Inject data into ejs template
--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,34 +1,29 @@
-import type { PluginOption } from 'vite';
-
+import { Plugin, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-// import GlobPlugin from 'vite-plugin-glob';
+
 // https://github.com/antfu/unocss
 // see unocss.config.ts for config
 import Unocss from 'unocss/vite';
+
 import { configHtmlPlugin } from './html';
-// import { configWindiCSSPlugin } from './windicss';
 import { configComponentsPlugin } from './components';
 import { configAutoImportPlugin } from './autoImport';
 import { configIconComponentPlugin } from './iconComponent';
 import { configMswPlugin } from './msw';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [
+  const vitePlugins: (Plugin | Plugin[])[] = [
     vue(),
+    splitVendorChunkPlugin(),
     vueJsx(),
-    // GlobPlugin(),
     Unocss(),
     configMswPlugin(viteEnv),
+    configComponentsPlugin(),
+    configAutoImportPlugin(),
+    configIconComponentPlugin(),
+    configHtmlPlugin(viteEnv, isBuild),
   ];
-
-  // vitePlugins.push(configWindiCSSPlugin());
-  vitePlugins.push(configComponentsPlugin());
-  vitePlugins.push(configAutoImportPlugin());
-  vitePlugins.push(configIconComponentPlugin());
-
-  // vite-plugin-html
-  vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
 
   return vitePlugins;
 }
