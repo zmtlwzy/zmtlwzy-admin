54311eb1c556b935ad73f52346b614f2f361136f
zmtlwzy <1025302051@qq.com>
2022-10-22 21:56:17 +0800

    chore: update

---
+++ src/composables/core/useContextLite.ts
@@ -0,0 +1,19 @@
+import type { InjectionKey } from 'vue'
+
+export default function useContext<T>(contextName = 'context') {
+  const injectKey: InjectionKey<T> = Symbol(contextName)
+
+  function useProvide(context: T) {
+    provide(injectKey, context)
+    return context
+  }
+
+  function useInject() {
+    return inject(injectKey) as T
+  }
+
+  return {
+    useProvide,
+    useInject,
+  }
+}
